09bbaede1f0dc48276fc9e7e5cec23590660bb20
prillyalda <19215285@bsi.ac.id>
2023-10-29 18:21:06 +0700

    detail mobil di halaman customer

--- application/controllers/customer/Dashboard.php
+++ application/controllers/customer/Dashboard.php
@@ -11,6 +11,14 @@ class Dashboard extends CI_Controller
 		$this->load->view('customer/dashboard', $data);
 		$this->load->view('templates_customer/footer');
 	}
+	public function detail_mobil($id)
+	{
+
+		$data['detail'] = $this->rental_model->ambil_id_mobil($id);
+		$this->load->view('templates_customer/header');
+		$this->load->view('customer/detail_mobil', $data);
+		$this->load->view('templates_customer/footer');
+	}
 
 }
 ?>

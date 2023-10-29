afe1452776ac9fd0c950bf65549ffb11fc815e00
prillyalda <19215285@bsi.ac.id>
2023-10-29 17:39:13 +0700

    Menampilkan Data Mobil di Halaman Customer

--- application/controllers/customer/Dashboard.php
+++ application/controllers/customer/Dashboard.php
@@ -1,12 +1,16 @@
 <?php
-	
-	class Dashboard extends CI_Controller{
-		public function index(){
 
-			$this->load->view('templates_customer/header');
-			$this->load->view('customer/dashboard', $data);
-			$this->load->view('templates_customer/footer');
-		}
+class Dashboard extends CI_Controller
+{
+	public function index()
+	{
+		// $data['mobil'] = $this->rental_model->get_data('mobil')->result();
+		$data['mobil'] = $this->db->query("SELECT * FROM mobil mb, type tp WHERE mb.kode_type=tp.kode_type")->result();
 
+		$this->load->view('templates_customer/header');
+		$this->load->view('customer/dashboard', $data);
+		$this->load->view('templates_customer/footer');
 	}
+
+}
 ?>

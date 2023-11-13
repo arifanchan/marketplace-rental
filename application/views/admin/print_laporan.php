69f31f856de971a0b40621a93aa886ea0c176d5b
ariefsama <muhariffi@gmail.com>
2023-11-13 22:38:58 +0700

    print laporan transaksi

--- application/views/admin/print_laporan.php
+++ application/views/admin/print_laporan.php
@@ -1,33 +1,18 @@
-<div class="main-content">
-    <section class="section">
-        <div class="section-header">
-            <h1>Filter Laporan Transaksi</h1>
-        </div>
-    </section>
 
-    <form method="POST" action="<?php echo base_url('admin/laporan') ?>">
-    	
-    	<div class="form-group">
-    		<label>Dari Tanggal</label>
-    		<input type="date" name="dari" class="form-control"></input>
-    		<?php echo form_error('dari','<span class="text-small text-danger">','</span>')  ?>
-    	</div>
+<h3 style="text-align: center">Laporan Transaksi Rental Mobil</h3>
 
-    	<div class="form-group">
-    		<label>Sampai Tanggal</label>
-    		<input type="date" name="sampai" class="form-control"></input>
-    		<?php echo form_error('sampai','<span class="text-small text-danger">','</span>')  ?>
-    	</div>
-
-    	<button type="submit" class="btn btn-primary"><i class="fas fa-eye"></i> Tampilkan Data</button>
-
-    </form>
-
-    <hr>
-
-    <div class="btn-group">
-        <a href="<?php echo base_url() . 'admin/laporan/print_laporan/?dari=' . set_value('dari') . '&sampai=' . set_value('sampai')?>" class="btn btn-sm btn-success" target="_blank"><i class="fas fa-print"></i> Print Laporan</a>
-    </div>
+<table>
+	<tr>
+		<td>Dari Tanggal</td>
+		<td>:</td>
+		<td><?php echo date('d-M-Y',strtotime($_GET['dari'])); ?></td>
+	</tr>
+		<tr>
+		<td>Sampai Tanggal</td>
+		<td>:</td>
+		<td><?php echo date('d-M-Y',strtotime($_GET['sampai'])); ?></td>
+	</tr>
+</table>
 
             <table class="table-responsive table table-striped table-bordered mt-3">
             <tr>
@@ -87,5 +72,8 @@
                     </tr>
 
             <?php endforeach; ?>
-        </table>
-</div>
+ 		</table>
+
+ 		<script type="text/javascript">
+ 			window.print();
+ 		</script>

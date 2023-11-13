1196e644318ea47e173b2643bb9fe7a142db5c3f
Brandon511 <brandon.jevri1@gmail.com>
2023-11-13 20:44:42 +0700

    Pembayaran

--- application/controllers/customer/Transaksi.php
+++ application/controllers/customer/Transaksi.php
@@ -9,5 +9,52 @@
 			$this->load->view('customer/transaksi',$data);
 			$this->load->view('templates_customer/footer');
 		}
+		public function pembayaran($id){
+			$data['transaksi'] = $this->db->query("SELECT * FROM transaksi tr, mobil mb, customer cs WHERE tr.id_mobil=mb.id_mobil AND tr.id_rental='$id' AND cs.nama_rental = tr.nama_rental ORDER BY id_rental DESC")->result();
+
+			$nama_rental = $data['transaksi'][0]->nama_rental;
+			$data['payment']	= $this->db->query("SELECT * FROM payment WHERE nama_rental = '$nama_rental'")->result();
+
+			$this->load->view('templates_customer/header');
+			$this->load->view('customer/pembayaran',$data);
+			$this->load->view('templates_customer/footer');
+		}
+		public function pembayaran_aksi(){
+			$id 				= $this->input->post('id_rental');
+			$bukti_pembayaran	= $_FILES['bukti_pembayaran']['name'];
+			
+			if($bukti_pembayaran){
+				$config['upload_path']		= './assets/upload';
+				$config['allowed_types']	= 'jpg|jpeg|png|tiff|pdf|webp';
+
+				$this->load->library('upload', $config);
+
+				if($this->upload->do_upload('bukti_pembayaran')){
+					$bukti_pembayaran = $this->upload->data('file_name');
+					$this->db->set('bukti_pembayaran', $bukti_pembayaran);
+				}else{
+					echo $this->upload->display_errors();
+				}
+			}
+
+			$data = array(
+				'bukti_pembayaran' => $bukti_pembayaran
+			);
+
+			$where = array(
+				'id_rental'			=> $id
+			);
+
+			$this->rental_model->update_data('transaksi', $data, $where);
+
+			$this->session->set_flashdata('pesan','<div class="alert alert-success alert-dismissible fade show" role="alert">
+				  Bukti Pembayaran Anda Berhasil di Upload
+				  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				    <span aria-hidden="true">&times;</span>
+				  </button>
+				</div>');
+			redirect('customer/transaksi');
+
+		}
     }
 ?>
